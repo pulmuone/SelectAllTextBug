73d255b4beb72f3463b7baf180cbafe65bc789b3
pulmuone <pulmuone.ji@gmail.com>
2024-09-06 06:48:03 +0900

    Add project files.

---
+++ SelectAllTextBug/SelectAllTextBug.iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+using Foundation;
+
+namespace SelectAllTextBug.iOS
+{
+    [Register(nameof(AppDelegate))]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
---
+++ SelectAllTextBug/SelectAllTextBug/MauiProgramExtensions.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+
+namespace SelectAllTextBug;
+
+public static class MauiProgramExtensions
+{
+	public static MauiAppBuilder UseSharedMauiApp(this MauiAppBuilder builder)
+	{
+		builder
+			.UseMauiApp<App>()
+            .UseMauiCommunityToolkit()
+            .ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+			});
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder;
+	}
+}
